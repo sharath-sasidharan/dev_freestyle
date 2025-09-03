dbdd39d64edb8f0322335e3a87e86ef93506d1aa
sarath <sasidharansharath13@gmail.com>
2025-09-03 15:52:30 +0530

    interim_4

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,41 @@
-const { test, expect } = require("@playwright/test");
+const { test, expect, request } = require("@playwright/test");
+
+const userPayload = {
+  postId: 1,
+  id: 1,
+  name: "MOses",
+  email: "gmail@gardner.biz",
+  body: "Jesus Is Great❤️😇🏆🙏🏻😘",
+};
 
 test("Google homepage has title", async ({ page }) => {
   await page.goto("https://google.com");
   await expect(page).toHaveTitle(/Google/);
+});
+
+test("GET REQUEST", async () => {
+  const apiRequest = await request.newContext();
+
+  const apiResponse = await apiRequest.get(
+    "https://jsonplaceholder.typicode.com/comments"
+  );
+
+  expect(apiResponse.ok()).toBeTruthy();
+  const json = await apiResponse.json();
+  console.log(json);
+});
+
+test("POST REQUEST", async () => {
+  const apiRequest = await request.newContext();
+
+  const apiResponse = await apiRequest.post(
+    "https://jsonplaceholder.typicode.com/comments",
+    {
+      data: userPayload,
+    }
+  );
+
+  expect(apiResponse.ok()).toBeTruthy();
+  const json = await apiResponse.json();
+  console.log(json);
 });
